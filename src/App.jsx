d1b5bf3cac6b240f1e6c1edc91def418704de61d
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-10-25 10:49:10 -0300

    hicimos la base para la segunda tarea, las actividades 1 y 2 serán entregadas como el mismo archivo cumpliendo ambas consignas, posiblemente

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Pelis web Diego Y Nico</h1>
-      <div className="card">
-      <img src="public/Terminator.jpg" alt="tattataad" />
+const fotos = [
+  './lal.jpg',
+  './lal2.jpg',
+  './lal3.pg'
+]
 
-      </div>
-      <p className="read-the-docs">
-      Bienvenido a Pelisweb, tu lugar para explorar el cine. Aquí encontrarás reseñas, trailers y noticias de estrenos, además de un catálogo que va desde clásicos hasta nuevas películas. Únete a nuestra comunidad de cinéfilos para compartir opiniones, debatir y recibir recomendaciones personalizadas. ¡Disfruta de la magia del cine!
-      </p>
-    </>
-  )
+return (
+<>
+    <h1>Pelis web Diego Y Nico</h1>
+    <div className="card">
+        <img src="public/Terminator.jpg" alt="tattataad" width="350" height="500" />
+
+    </div>
+    <div>
+        <p className="read-the-docs">
+            Bienvenido a Pelisweb, tu lugar para explorar el cine. Aquí encontrarás reseñas, trailers y noticias de
+            estrenos, además de un catálogo que va desde clásicos hasta nuevas películas. Únete a nuestra comunidad de
+            cinéfilos para compartir opiniones, debatir y recibir recomendaciones personalizadas. ¡Disfruta de la magia
+            del cine!
+        </p>
+
+        <h2>Las pelis disponibles son:</h2>
+    </div>
+    <div>
+      ala borger
+      <button>Foto 1</button>
+      <button>Foto 2</button>
+      <button>Foto 3</button>
+    </div>
+</>
+)
 }
 
 export default App
